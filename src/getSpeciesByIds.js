e69023d4f2b3c8a0e63937e45b45bae43506b850
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2021-10-19 16:25:12 -0300

    Adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const findSpeciesById = ids.map((idToFind) => species.find((animal) => animal.id === idToFind));
+  return findSpeciesById;
 }
 
 module.exports = getSpeciesByIds;
