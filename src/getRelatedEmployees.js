507e484973c7c4ed9ae44f583ae08bd998c0e43e
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2021-10-21 10:06:42 -0300

    Adiciona requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) !== true) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const employeeList = employees.filter((employee) => employee.managers[0] === managerId);
+  const employeeName = employeeList.map((employee) => `${employee.firstName} ${employee.lastName}`);
+  return employeeName;
 }
 
 module.exports = { isManager, getRelatedEmployees };
