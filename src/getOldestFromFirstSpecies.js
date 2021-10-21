2b5b5c83724201d025a9e55e36bbdb38fccbffb0
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2021-10-21 12:08:55 -0300

    Adiciona requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animal = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const olderAge = species.find((specie) => specie.id === animal).residents.map((age) => age.age)
+    .sort((age1, age2) => age2 - age1)[0];
+  const result = species.find((specie) => specie.id === animal).residents
+    .find((resident) => resident.age === olderAge);
+  return Object.values(result);
 }
 
 module.exports = getOldestFromFirstSpecies;
