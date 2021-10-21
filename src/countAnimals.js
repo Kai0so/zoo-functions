81901c60533ed42feb50323408fe97591d05057a
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2021-10-21 10:41:17 -0300

    Adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const animalList = {};
+    data.species.forEach((specieName) => {
+      animalList[specieName.name] = specieName.residents.length;
+    });
+    return animalList;
+  }
+  const { specie, sex } = animal;
+  if (!sex) {
+    const animalSpecieName = species
+      .find((animalSpecie) => animalSpecie.name === specie).residents.length;
+    return animalSpecieName;
+  }
+  const animalSex = species.find((animalSpecie) => animalSpecie.name === specie).residents
+    .filter((residentAnimal) => residentAnimal.sex === sex).length;
+  return animalSex;
 }
 
 module.exports = countAnimals;
