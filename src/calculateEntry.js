85c788fa2ca68c2f6dde6967060d9b4cecf20e86
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2021-10-21 11:44:53 -0300

    Adiciona requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const peopleObject = {};
+  const childs = entrants.filter((entrant) => entrant.age < 18).length;
+  peopleObject.child = childs;
+  const adults = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  peopleObject.adult = adults;
+  const seniors = entrants.filter((entrant) => entrant.age >= 50).length;
+  peopleObject.senior = seniors;
+  return peopleObject;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  let totalEntryValue = 0;
+  if (entrants && Object.keys(entrants).length !== 0) {
+    const childTotalPrice = countEntrants(entrants).child * data.prices.child;
+    const adultTotalPrice = countEntrants(entrants).adult * data.prices.adult;
+    const seniorTotalPrice = countEntrants(entrants).senior * data.prices.senior;
+    totalEntryValue = childTotalPrice + adultTotalPrice + seniorTotalPrice;
+  }
+  return totalEntryValue;
 }
 
 module.exports = { calculateEntry, countEntrants };
