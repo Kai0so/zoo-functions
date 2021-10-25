0d024d5a668cfbc07a7da69f48317274a2df06c0
Kaio Militão <kaioaugustoalmeida.m@gmail.com>
2021-10-25 04:47:14 -0300

    Adiciona requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,43 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const findAnimal = (person) => species.filter((specie) =>
+  person.includes(specie.id)).map((specie) => specie.name);
+
+const findLocation = (person) => species.filter((specie) =>
+  person.includes(specie.id)).map((specie) => specie.location);
+
+const getEmployees = (options) => {
+  if (options === undefined) {
+    const objectArray = employees.map((person) => ({
+      id: person.id,
+      fullName: `${person.firstName} ${person.lastName}`,
+      species: findAnimal(person.responsibleFor),
+      locations: findLocation(person.responsibleFor),
+    }));
+    return objectArray;
+  }
+};
+
+const findPersonById = (options) => {
+  const nameContains = employees.some((person) => person.id === options.id
+    || person.firstName === options.name || person.lastName === options.name);
+  if (nameContains === true) {
+    const employee = employees.find((person) => person.id === options.id
+    || person.firstName === options.name || person.lastName === options.name);
+    const employeeObject = {
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: findAnimal(employee.responsibleFor),
+      locations: findLocation(employee.responsibleFor),
+    };
+    return employeeObject;
+  }
+  throw new Error('Informações inválidas');
+};
+
+function getEmployeesCoverage(options) {
+  return getEmployees(options) || findPersonById(options);
 }
 
 module.exports = getEmployeesCoverage;
